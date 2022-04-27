3bc03ba46ed8233a9e3c5098512ade41fde5d6fe
Lucas <lucas_bremm@hotmail.com>
2022-04-27 20:56:08 -0300

    Key spawning in different locations

--- Assets/Scripts/KeyBehaviour.cs
+++ Assets/Scripts/KeyBehaviour.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class KeyBehaviour : MonoBehaviour
 {
+  Transform[] locations;
+
+  void Start() {
+    locations = GetComponentsInChildren<Transform>();
+
+    int index = Random.Range(0, locations.Length);
+
+    transform.position = locations[index].position;  
+  }
+
   void OnTriggerEnter2D(Collider2D other) {
     if (other.tag == "Player") {
       GameManager.instance.GotKey();
